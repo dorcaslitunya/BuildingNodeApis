c09d5cdb76969f31c48202e09850bf6635cb4357
dorcaslitunya <anonolitunya@gmail.com>
2022-11-08 09:52:35 +0300

    Placing a relationship between products and orders

--- api/routes/order.js
+++ api/routes/order.js
@@ -2,21 +2,57 @@ const express = require ('express');
 
 const router = express.Router();
 const mongoose = require('mongoose');
+const order = require('../../models/order');
 
 const Order = require ('../../models/order');
+
+//used as a check to make sure we only create orders for which
+//products exist
+const Product = require('../../models/product');
 
 
 
 
 router.get('/',(req,res,next) => {
-
+ Order.find()
+ .select('product quantity_id')
+ .exec()
+ .then(docs =>{
     res.status(200).json({
-        message:"Handling GET requests to /order"
+        count:docs.length,
+        orders:docs.map(doc=>{
+            return{
+                _id:doc.id,
+                product:doc.product,
+                quantity:doc.quantity,
+                request:{
+            type:'GET',
+            url:"http://localhost:3000/order/"+doc._id 
+                  }  }
+
+        }),
+        
+      
     });
+ })
+ .catch(err=>{
+    res.status(500).json({
+        error:err
+    });
+ })
 })
 
 router.post('/',(req,res,next) => {
-    const order1 =new Order({
+    //returns product if found and returns null if product is not found
+    Product.findById(req.body.productId)
+    .then(productFound =>{
+        if(!productFound){
+            return res.status(404).json({
+                message:"Product not found",
+
+            })
+        }
+        const order1 =new Order({
         _id:new mongoose.Types.ObjectId(),
         quantity:req.body.quantity,
         //Id of the product we are connected to
@@ -25,46 +61,86 @@ router.post('/',(req,res,next) => {
 
     //Since this is not a query, it runs an actuall promise thus no need
     //to  add exec()
-    order1.save()
-    .then(result =>{
-        console.log(result);
-        res.status(201).json(result);
-    })
-    .catch(err=>{
-        console.log(err);
-        res.status(500).json({
-            error:err
-        });
-    })
 
+    //save it..then now do another chaining outside current then() 
+    //to avoid internal chainings
+    // execution continues as .save returns a promise
+
+   return  order1.save()
+    
+}).then(result =>{
+    console.log(result);
+    res.status(201).json({
+        message:"Order stored",
+        createdObject:{
+            Orderid: result._id,
+            quantity:result.quantity,
+            product:result.product   
+        },
+        request:{
+            type:'GET',
+            url:"http://localhost:3000/order/" + result._id 
+        }
+    });
+})
+.catch(err=>{
+    console.log(err);
+    res.status(500).json({
+        message:"Product not found",
+        error:err
+    });
+})
+    
     
 })
 
 router.get('/:ordersid',(req,res,next)=>{
-    const id=req.params.ordersid;
-   if(id==='special') {
-    res.status(200).json({
-        message:"You discoverd the special ORDER ID",
-        id:id
-    });
-}else{
-    res.status(200).json({
-        message:"You passed an ORDER id"
-    });
-}
+    Order.findById(req.params.ordersid).exec()
+    .then(orderFound=>{
+        if(!orderFound){
+            return res.status(404).json({
+                "message":"Order not found"
+            })
+        }
+        res.status(200).json({
+            orders:orderFound,
+            request:{
+                type:'GET ',
+                url:"http://localhost:3000/order/"
+            }
+
+
+        })
+    })
+    .catch(err=>{
+        res.status(500).json({
+            error:err
+        })
+    })
 });
 router.delete('/:orderid',(req,res,next)=>{
-    const id=req.params.orderid;
-   if(id==='special') {
-    res.status(200).json({
-        message:"You discoverd the special ORDER delete ID",
-        id:id
-    });
-}else{
-    res.status(200).json({
-        message:"You passed an ORDER delete id"
-    });
-}
+    Order.deleteOne({_id: req.params.orderid}).exec()
+    .then(result=>{
+        if(result.deletedCount==0){
+            return res.status(404).json({
+                message:"No such order exists"
+            })
+        }
+        res.status(200).json({
+            message:"Order deleted",
+            
+            request:{
+                type:'POST',
+                url:"http://localhost:3000/order/",
+                body:{productId:'ID', quantity:"Number"}
+            }
+        })
+    })
+    .catch(err=>{
+        res.status(500).json({
+            error:err
+        })
+    })
 });
 
 module.exports = router;
